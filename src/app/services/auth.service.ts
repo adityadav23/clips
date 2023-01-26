d17767e545dfb3f1837dd1590523c757cc3df51f
Aditya Yadav <aditya.yadav@dhwaniris.com>
2023-01-26 20:43:40 +0530

    Destroying auth modal after login/register with 1sec delay

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,7 +3,7 @@ import {AngularFireAuth} from '@angular/fire/compat/auth';
 import {AngularFirestore, AngularFirestoreCollection}  from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import IUser from '../models/user.model'
-import {map} from 'rxjs/operators'
+import {delay, map} from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,7 @@ export class AuthService {
 
   private usersCollection: AngularFirestoreCollection<IUser>
   public isAuthenticated$: Observable<boolean>
+  public isAuthenticatedWithDelay$: Observable<boolean>
 
   constructor(
     private auth: AngularFireAuth, 
@@ -21,6 +22,9 @@ export class AuthService {
   // auth.user.subscribe(console.log)
   this.isAuthenticated$ = auth.user.pipe(
     map( user=> !!user)
+  )
+  this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
+    delay(1000)
   )
 }
 
