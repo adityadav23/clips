b8562ac762510a5cc0fff5d52b3892b8c9892ec9
Aditya Yadav <aditya.yadav@dhwaniris.com>
2023-01-26 20:07:34 +0530

    login using firebase auth

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {AngularFireAuth } from '@angular/fire/compat/auth'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,11 +7,43 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(
+    private auth:AngularFireAuth
+  ){ }
+
   credentials = {
     email: "",
     password: ""
   }
-  login(){
-    console.log(this.credentials)
+  showAlert = false;
+  alertMsg = "Please wait!. Weare logging you in."
+  alertColor =  "blue";
+  inSubmission = false;
+
+  async login(){
+
+    this.showAlert = true;
+    this.alertMsg = "Please wait!. Weare logging you in."
+    this.alertColor =  "blue";
+    this.inSubmission = true;
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      )
+    } catch (error) {
+      
+      console.error(error);
+      
+      this.alertMsg = 'An Unexpected error occurred! Please try again later.';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+
+
+    this.alertMsg = 'Success! Your account has been created.';
+    this.alertColor = 'green';
   }
 }
