e49016fe5554c77cf730d663b6573e48488d1a33
Aditya Yadav <aditya.yadav@dhwaniris.com>
2023-01-26 18:57:08 +0530

    Initializaing firebase before angular to check authentication

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
+/* Initializing the firebase app before angular. */
+firebase.initializeApp(environment.firebase);
+/* A variable that is used to prevent the app from being bootstrapped multiple times. */
+let appInit = false;
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+/* A listener that waits for the user to be authenticated before bootstrapping the app. */
+firebase.auth().onAuthStateChanged(() => {
+  if (!appInit) {
+
+    platformBrowserDynamic()
+      .bootstrapModule(AppModule)
+      .catch((err) => console.error(err));
+  }
+  appInit = true;
+});
