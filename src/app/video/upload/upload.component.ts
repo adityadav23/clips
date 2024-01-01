49cc26819f5d8f0c61a994f0917d6e1ef5190ba8
Aditya Yadav <aditya.yadav@dhwaniris.comm>
2024-01-01 11:40:29 +0530

    added input element to upload file

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/compat/storage';
 import {v4 as uuid} from 'uuid'
 import {last, switchMap} from 'rxjs/operators'
 import { AngularFireAuth } from '@angular/fire/compat/auth';
@@ -12,7 +12,7 @@ import {ClipService} from '../../services/clip.service'
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
-export class UploadComponent {
+export class UploadComponent implements OnDestroy {
   
   isDragOver = false;
   file: File | null =  null;
@@ -24,7 +24,7 @@ export class UploadComponent {
   percentage = 0;
   showPercentage = false;
   user:  firebase.User | null =  null
-  
+  task?: AngularFireUploadTask
   constructor(
     private storage: AngularFireStorage,
     private auth: AngularFireAuth,
@@ -32,7 +32,11 @@ export class UploadComponent {
   ){
     auth.user.subscribe(user=> this.user = user)
   }
-
+  
+  ngOnDestroy(): void {
+    this.task?.cancel(); //cancels API request if omponent destroyed
+  }
+  
   title = new FormControl('', [
     Validators.required,
     Validators.minLength(3)
@@ -69,14 +73,14 @@ item(0) property to the file property. */
     const clipFileName = uuid()
     const clipPath = `clips/${clipFileName}.mp4`
     
-    const task = this.storage.upload(clipPath, this.file)
+    this.task = this.storage.upload(clipPath, this.file)
     const clipRef = this.storage.ref(clipPath);
 
-    task.percentageChanges().subscribe(progress => {
+    this.task.percentageChanges().subscribe(progress => {
       this.percentage =  (progress as number)/100
     })
     // console.log("File Uploaded!")
-    task.snapshotChanges().pipe(
+    this.task.snapshotChanges().pipe(
       last(),
       switchMap(()=> clipRef.getDownloadURL())
     ).subscribe({
